3fff813a2a62d0fd26bd85d3417f14e8fe84d932
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-10-01 20:07:43 -0300

    Google

--- index.js
+++ index.js
@@ -2,25 +2,14 @@ const elemRoot = document.querySelector('#root')
 
 const fontSize = {
   initial: 2,
-  final: 200
+  final: 20
 }
 
 let template = ''
 
 for(let i = fontSize.initial; i < fontSize.final; i++) {
   template += `
-    <div class="ehash-logo" style="font-size: ${i}px;">
-      <div class="ehash-logo__head">
-        <div class="ehash-logo__head--top-point"></div>
-        <div class="ehash-logo__head--bottom-point"></div>
-      </div>
-      <div class="ehash-logo__body">
-        <span class="ehash-logo__body--text">E</span>
-        <div class="ehash-logo__body__hash">
-          <span class="ehash-logo__body__hash--text">#</span>
-        </div>
-      </div>
-    </div>
+    <div class="ui-clone-google" style="font-size: ${i}px;"></div>
   `;
 }
 
