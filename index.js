15213d2ace04aae23d896948295dd332471dc050
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-10-02 20:39:48 -0300

    Google Chrome

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const elemRoot = document.querySelector('#root')
 
 const fontSize = {
   initial: 2,
-  final: 20
+  final: 100
 }
 
 let template = ''
 
 for(let i = fontSize.initial; i < fontSize.final; i++) {
   template += `
-    <div class="ui-clone-google" style="font-size: ${i}px;"></div>
+    <div class="ui-clone-chrome" style="font-size: ${i}px;">
+      <div class="ui-clone-chrome-circle"></div>
+      <div class="ui-clone-chrome-yellow"></div>
+      <div class="ui-clone-chrome-green"></div>
+    </div>
   `;
 }
 
