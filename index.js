6d8c6b875c1b0dfd91db4708fc9d588de227e980
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-10-03 13:03:18 -0300

    Netflix

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const elemRoot = document.querySelector('#root')
 
 const fontSize = {
-  initial: 2,
-  final: 100
+  initial: 10,
+  final: 25
 }
 
 let template = ''
 
 for(let i = fontSize.initial; i < fontSize.final; i++) {
   template += `
-    <div class="ui-clone-chrome" style="font-size: ${i}px;">
-      <div class="ui-clone-chrome-circle"></div>
-      <div class="ui-clone-chrome-yellow"></div>
-      <div class="ui-clone-chrome-green"></div>
+    <div class="ui-clone-netflix" style="font-size: ${i}px;">
+      <div>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <h3 class="ui-clone-netflix--title">Netflix</h3>
     </div>
   `;
 }
